3d602dd8219d4053738bad24a194f16d53c28a71
Ddunghip <15a10010289@students.hou.edu.vn>
2022-03-09 17:49:52 +0700

    test3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav';
+
 // template + logic
 // jsx
 function App() {
   let link = 'https://www.youtube.com/watch?v=2hKDGAcHw5M';
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello</h1>
