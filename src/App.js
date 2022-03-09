1bb5a0460c12c98da9724fd6dd10020b68f1febb
Ddunghip <15a10010289@students.hou.edu.vn>
2022-03-09 17:37:11 +0700

    test2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 // template + logic
 // jsx
 function App() {
+  let link = 'https://www.youtube.com/watch?v=2hKDGAcHw5M';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello</h1>
+        <a href={link} target='_blank'>Click me</a>
       </header>
     </div>
   );
